ce27fd536773fcd7b868547c3e71440849828839
matyasfric <matyas.fric@gmail.com>
2024-04-19 11:35:20 +0200

    uprava fce pro zmenu pozice textu

--- script.js
+++ script.js
@@ -3,11 +3,29 @@ function moveText() {
     const text = document.getElementById('bendingText');
     const maxX = window.innerWidth - text.clientWidth;
     const maxY = window.innerHeight - text.clientHeight;
-    const newX = Math.random() * maxX;
-    const newY = Math.random() * maxY;
+
+    // Nová pozice textu
+    let newX = Math.random() * maxX;
+    let newY = Math.random() * maxY;
+
+    // Kontrola, zda nová pozice nepřesahuje okno prohlížeče
+    if (newX < 0) {
+        newX = 0; // Pokud nová pozice přesahuje zleva, nastavíme na 0
+    }
+    if (newY < 0) {
+        newY = 0; // Pokud nová pozice přesahuje shora, nastavíme na 0
+    }
+    if (newX > maxX) {
+        newX = maxX; // Pokud nová pozice přesahuje zprava, nastavíme na maxX
+    }
+    if (newY > maxY) {
+        newY = maxY; // Pokud nová pozice přesahuje zdola, nastavíme na maxY
+    }
+
     text.style.left = `${newX}px`;
     text.style.top = `${newY}px`;
 }
+
 
 // Přidání události pro pohyb myši nad textem
 document.getElementById('bendingText').addEventListener('mouseenter', moveText);
